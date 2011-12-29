143084c975fada6b2a141cab751337092ad7a21c
Krzysztof Szczęsny <spooky9@poczta.onet.pl>
2011-12-29 22:05:09 +0100

    initial commit

---
+++ jquery.fooPlugin.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+
+    $.fn.fooPlugin = function (customOptions) {
+        var options = $.extend({}, $.fn.fooPlugin.defaultOptions, customOptions),
+        globalVar = -1;
+
+        function _init(matchedObj) {
+            // construction logic here
+        };
+
+        return this.each(function () {
+            var $this = $(this);
+            _init($this);
+        });
+    };
+
+    $.fn.fooPlugin.defaultOptions = {
+        sampleOption: 'bar'
+    };
+
+})(jQuery);
